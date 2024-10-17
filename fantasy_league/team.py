62195c7b659be50429ff7f3eff04423c3f6c93ec
matt58k <matteo.guill@hotmail.fr>
2024-10-17 08:46:57 +0200

    uptdate encore

--- fantasy_league/team.py
+++ fantasy_league/team.py
@@ -16,14 +16,14 @@ class Team:
     
     def add_player(self, player:Player):
         self.players.append(player)
-        print(f"The player {player} was added to the team {self.team_name}")
+        print(f"The player {player.name} was added to the team {self.team_name}")
     
     def remove_player(self, player:Player):
         if player in self.players:
             self.players.remove(player)
             print(f"The player {player.name} was removed from the team {self.team_name}.")
         else:
-            print(f"The player {player} is not in the team {self.team_name}.")
+            print(f"The player {player.name} is not in the team {self.team_name}.")
     
     def __str__(self):
-        return f"The team {self.team_name} has {', '.join(self.players)}"
+        return f"The team {self.team_name} has {', '.join(self.players.player.name)}"
