03909a33ba0152cae522ecace986ef7c9fd0aa9a
matt58k <matteo.guill@hotmail.fr>
2024-10-17 08:44:54 +0200

    uptdate

--- fantasy_league/team.py
+++ fantasy_league/team.py
@@ -4,23 +4,24 @@ Created on Thu Oct 10 08:56:07 2024
 
 @author: matte
 """
+from typing import List
 
 class Team:
     team_name:str
-    players:list=None
+    players:List[Player]
     
     def __init__(self, team_name, players=[]):
         self.team_name = team_name
-        self.players = players if players is not None else []
+        self.players = players
     
-    def add_player(self, player:str):
+    def add_player(self, player:Player):
         self.players.append(player)
         print(f"The player {player} was added to the team {self.team_name}")
     
-    def remove_player(self, player:str):
+    def remove_player(self, player:Player):
         if player in self.players:
             self.players.remove(player)
-            print(f"The player {player} was removed from the team {self.team_name}.")
+            print(f"The player {player.name} was removed from the team {self.team_name}.")
         else:
             print(f"The player {player} is not in the team {self.team_name}.")
     
