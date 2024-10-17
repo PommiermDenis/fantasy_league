67e4fff9e8dd3aa2996dfb129d69e3c85227c3c5
matt58k <matteo.guill@hotmail.fr>
2024-10-17 09:20:20 +0200

    faut vraiment accepter c'est hyper urgent !!!!!!!

--- fantasy_league/main.py
+++ fantasy_league/main.py
@@ -8,8 +8,9 @@ Created on Thu Oct 10 08:56:53 2024
 
 import pandas as pd
 from player import Player
-from league import League
 from team import Team
+from league import League
+
 
 df = pd.read_csv("players.csv")
 
